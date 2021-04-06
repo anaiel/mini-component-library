ef5273f5062c4c57d08318dc91e26b07a5f67ec5
anaiel <an.leclabart@gmail.com>
2021-04-06 21:15:25 +0200

    :sparkles: complete select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import { COLORS } from "../../constants";
@@ -10,32 +10,47 @@ const Select = ({ value, onChange, children }) => {
 
   return (
     <Wrapper>
-      <SelectWrapper value={value} onChange={onChange}>
+      <InvisibleSelect value={value} onChange={onChange}>
         {children}
-      </SelectWrapper>
-      <Icon
-        id="chevron-down"
-        size="24"
-        strokeWidth="2"
-        style={{
-          position: "absolute",
-          top: "calc(50% - 12px)",
-          right: "11px",
-          pointerEvents: "none",
-        }}
-        aria-hidden="true"
-      />
+      </InvisibleSelect>
+      <DisplayedValueWrapper aria-hidden={true}>
+        {displayedValue ?? children[0]}
+        <Icon
+          id="chevron-down"
+          size="24"
+          strokeWidth="2"
+          style={{
+            position: "absolute",
+            top: "calc(50% - 12px)",
+            right: "11px",
+            pointerEvents: "none",
+          }}
+          aria-hidden="true"
+        />
+      </DisplayedValueWrapper>
     </Wrapper>
   );
 };
 
-const SelectWrapper = styled.select`
-  appearance: none; /* Remove default arrow */
+const DisplayedValueWrapper = styled.div`
+  isolation: isolate;
+  position: relative;
   background-color: ${COLORS.transparentGray15};
   color: inherit;
   border-radius: 8px;
   border: none;
   padding: 12px 62px 12px 16px;
+  pointer-events: none;
+`;
+
+const InvisibleSelect = styled.select`
+  appearance: none; /* Remove default arrow */
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  right: 0;
+  opacity: 0;
 `;
 
 const Wrapper = styled.div`
